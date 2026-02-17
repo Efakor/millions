d1a8847040f18857ea47ae8d99bf427a421d48cb
archimakwana <archiamakwana@gmail.com>
2026-02-17 17:16:47 +0100

    [FEAT] Add Share class with fields, getters and validation

--- millions/src/main/java/edu/ntnu/idi/idatt2003/Share.java
+++ millions/src/main/java/edu/ntnu/idi/idatt2003/Share.java
@@ -1,11 +1,73 @@
 package edu.ntnu.idi.idatt2003;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.Objects;
+
 
 public class Share {
-  private Stock stock;
-  String company;
-  private final List<BigDecimal> prices;
+  private final Stock stock;
+  private final BigDecimal quantity;
+  private final  BigDecimal purchasePrice ;
+
+  public Share(Stock stock,BigDecimal quantity,BigDecimal purchasePrice){
+    validateStock(stock);
+    validateQuantity(quantity);
+    validatePurchasePrice(purchasePrice);
+
+    this.stock=stock;
+    this.quantity=quantity;
+    this.purchasePrice=purchasePrice;
+
+
+  }
+  public Stock getStock() {
+    return stock;
+  }
+  public BigDecimal getQuantity() {
+    return quantity;
+  }
+  public BigDecimal getPurchasePrice() {
+    return purchasePrice;
+  }
+  private void validateStock(Stock stock){
+    if(stock==null){
+      throw new IllegalArgumentException("stock is null");
+    }
+  }
+  private void validateQuantity(BigDecimal quantity){
+    if(quantity==null){
+      throw new IllegalArgumentException("quantity is null");
+    }
+    if(quantity.compareTo(BigDecimal.ZERO)<=0){
+      throw new IllegalArgumentException("quantity must be greater than zero");
+    }
+  }
+  private void validatePurchasePrice(BigDecimal purchasePrice){
+    if(purchasePrice==null){
+      throw new IllegalArgumentException("purchasePrice is null");
+    }if(purchasePrice.compareTo(BigDecimal.ZERO)<=0){
+      throw new IllegalArgumentException("purchasePrice must be greater than zero");
+    }
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Share share = (Share) o;
+    return Objects.equals(stock, share.stock) && Objects.equals(purchasePrice, share.purchasePrice);
+  }
+  @Override
+  public int hashCode() {
+    return Objects.hash(stock,quantity, purchasePrice);
+  }
+  @Override
+  public String toString(){
+    return String.format("Stock: %s, Quantity: %s, PurchasePrice: %s",
+        stock.getSymbol(), quantity, purchasePrice);
+  }
+
 
 }
