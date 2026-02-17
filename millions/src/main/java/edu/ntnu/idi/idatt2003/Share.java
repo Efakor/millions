834bc7a45b83bbb790f57dddc3a606efbb190db6
archimakwana <archiamakwana@gmail.com>
2026-02-17 16:45:54 +0100

    [DOCS] Fix JavaDoc descriptions in Stock class

--- millions/src/main/java/edu/ntnu/idi/idatt2003/Share.java
+++ millions/src/main/java/edu/ntnu/idi/idatt2003/Share.java
@@ -1,5 +1,11 @@
 package edu.ntnu.idi.idatt2003;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Share {
-  public Share() {}
+  private Stock stock;
+  String company;
+  private final List<BigDecimal> prices;
+
 }
