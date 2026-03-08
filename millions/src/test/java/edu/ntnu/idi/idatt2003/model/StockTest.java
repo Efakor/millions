e9b60e5af53ed995a40161e86635c8988feda844
archimakwana <archiamakwana@gmail.com>
2026-03-08 15:32:32 +0100

    [Refactor] Added @Nested in the the StockTest.java

--- millions/src/test/java/edu/ntnu/idi/idatt2003/model/StockTest.java
+++ millions/src/test/java/edu/ntnu/idi/idatt2003/model/StockTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 
@@ -18,227 +19,224 @@ class StockTest {
   }
 
   //Constructor Tests
-  @Test
-  void testConstructorWithValidInputs() {
-    //Arrange & Act
-    Stock stock = new Stock("GOOGL", "Alphabet Inc.", new BigDecimal("2800.00"));
-    //Assert
-    assertEquals("GOOGL", stock.getSymbol());
-    assertEquals("Alphabet Inc.", stock.getCompany());
-    assertEquals(new BigDecimal("2800.00"), stock.getSalesPrice());
-  }
+  @Nested
+  class ConstructorValid{
+    @Test
+    void testConstructorWithValidInputs() {
+      //Arrange & Act
+      Stock stock = new Stock("GOOGL", "Alphabet Inc.", new BigDecimal("2800.00"));
+      //Assert
+      assertEquals("GOOGL", stock.getSymbol());
+      assertEquals("Alphabet Inc.", stock.getCompany());
+      assertEquals(new BigDecimal("2800.00"), stock.getSalesPrice());
+    }
+    @Test
+    void testConstructorWithNullSymbols() {
+      //Arrange,Assert and Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock(null, "Apple Inc.", new BigDecimal("2800.00")));
+      assertTrue(exception.getMessage().contains("Stock symbol"));
+    }
+    @Test
+    void testConstructorWithEmptySymbols() {
+      //Arrange,Assert  and Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock("", "Apple Inc.", new BigDecimal("2800.00")));
+      assertTrue(exception.getMessage().contains("Stock symbol"));
 
-  @Test
-  void testConstructorWithNullSymbols() {
-    //Arrange,Assert and Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock(null, "Apple Inc.", new BigDecimal("2800.00")));
-    assertTrue(exception.getMessage().contains("Stock symbol"));
-  }
-
-  @Test
-  void testConstructorWithEmptySymbols() {
-    //Arrange,Assert  and Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock("", "Apple Inc.", new BigDecimal("2800.00")));
-    assertTrue(exception.getMessage().contains("Stock symbol"));
-
-  }
-
-  @Test
-  void testConstructorWithBlankSymbols() {
-    //Arrange,Assert and  Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock("  ", "Apple Inc.", new BigDecimal("2800.00")));
-    assertTrue(exception.getMessage().contains("Stock symbol"));
+    }
+    @Test
+    void testConstructorWithBlankSymbols() {
+      //Arrange,Assert and  Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock("  ", "Apple Inc.", new BigDecimal("2800.00")));
+      assertTrue(exception.getMessage().contains("Stock symbol"));
 
 
-  }
-
-  @Test
-  void testConstructorWithNullCompany() {
-    //Arrange,Assert and  Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock(" AAPL ", null, new BigDecimal("150.50")));
-    assertTrue(exception.getMessage().contains("Company name"));
-
-  }
-
-  @Test
-  void testConstructorWithEmptyCompany() {
-    //Arrange,Assert and  Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock(" AAPL ", "", new BigDecimal("150.50")));
-    assertTrue(exception.getMessage().contains("Company name"));
-
-  }
-
-  @Test
-  void testConstructorWithNullSalesPrice() {
-    //Arrange,Assert and  Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock(" AAPL ", "Apple Inc.", null));
-    assertTrue(exception.getMessage().contains("Sales price"));
-
-  }
-
-  @Test
-  void testConstructorWithZeroSalesPrice() {
-    //Arrange,Assert and  Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock(" AAPL ", "Apple Inc.", BigDecimal.ZERO));
-    assertTrue(exception.getMessage().contains("Sales"));
-
-  }
-
-  @Test
-  void testConstructorWithNegativeSalesPrice() {
-    //Arrange,Assert and  Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> new Stock(" AAPL ", "Apple Inc.", new BigDecimal("-150.50")));
-    assertTrue(exception.getMessage().contains("Sales"));
+    }
 
 
   }
 
-  //Getter tests
-  @Test
-  void testGetSymbol() {
-    //Arrange and Act
-    String symbol = appleStock.getSymbol();
-    //Assert
-    assertEquals("AAPL", symbol);
-  }
+  /**
+   * Test for Stock construction validation, company name errors.
+   */
+  @Nested
+  class ConstructorCompanyNameValidationTests{
+    @Test
+    void testConstructorWithNullCompany() {
+      //Arrange,Assert and  Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock(" AAPL ", null, new BigDecimal("150.50")));
+      assertTrue(exception.getMessage().contains("Company name"));
 
-  @Test
-  void testGetCompany() {
-    //Arrange and Act
-    String company = appleStock.getCompany();
-    //Assert
-    assertEquals("Apple Inc.", appleStock.getCompany());
-  }
+    }
+    @Test
+    void testConstructorWithEmptyCompany() {
+      //Arrange,Assert and  Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock(" AAPL ", "", new BigDecimal("150.50")));
+      assertTrue(exception.getMessage().contains("Company name"));
 
-
-  @Test
-  void testGetSalesPriceWithReturnsInitialPrice() {
-    //Arrange and Act
-    BigDecimal salesPrice = appleStock.getSalesPrice();
-    //Assert
-    assertEquals(initialPrice, salesPrice);
-  }
-
-  @Test
-  void testGetSalesPriceReturnsLatestPrice() {
-    //Arrange
-    BigDecimal newPrice = new BigDecimal("155.50");
-    appleStock.addNewSalesPrice(newPrice);
-    //Act
-    BigDecimal salesPrice = appleStock.getSalesPrice();
-    //Assert
-    assertEquals(newPrice, salesPrice);
+    }
 
   }
+  @Nested
+  class ConstructorSalesPriceValidationTests{
+    @Test
+    void testConstructorWithNullSalesPrice() {
+      //Arrange,Assert and  Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock(" AAPL ", "Apple Inc.", null));
+      assertTrue(exception.getMessage().contains("Sales price"));
 
-  @Test
-  void testAddNewSalesPriceWithValidPrice() {
-    //Arrange
-    BigDecimal newPrice = new BigDecimal("160.50");
-    //Act
-    appleStock.addNewSalesPrice(newPrice);
-    //Assert
-    assertEquals(newPrice, appleStock.getSalesPrice());
+    }
+    @Test
+    void testConstructorWithZeroSalesPrice() {
+      //Arrange,Assert and  Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock(" AAPL ", "Apple Inc.", BigDecimal.ZERO));
+      assertTrue(exception.getMessage().contains("Sales"));
+
+    }
+    @Test
+    void testConstructorWithNegativeSalesPrice() {
+      //Arrange,Assert and  Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> new Stock(" AAPL ", "Apple Inc.", new BigDecimal("-150.50")));
+      assertTrue(exception.getMessage().contains("Sales"));
 
 
-  }
-  @Test
-  void testAddNewSalesPriceWithNullPrice() {
-    //Arrange and Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> appleStock.addNewSalesPrice(null));
-    assertTrue(exception.getMessage().contains("Sales price"));
+    }
 
-  }
-  @Test
-  void testaddNewSalesPriceWithZeroPrice() {
-    //Arrange and Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> appleStock.addNewSalesPrice(BigDecimal.ZERO));
-    //Assert
-    assertTrue(exception.getMessage().contains("Sales"));
-  }
-  @Test
-  void testaddNewSalesPriceWithNegativePrice() {
-    //Arrange and Act
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-        () -> appleStock.addNewSalesPrice(new BigDecimal("-150.50")));
-
-    //Assert
-    assertTrue(exception.getMessage().contains("Sales"));
 
   }
-  @Test
-  void testAddMultiplePrices() {
-    BigDecimal price2 = new BigDecimal("158.50");
-    BigDecimal price3 = new BigDecimal("160.50");
-    BigDecimal price4 = new BigDecimal("164.50");
+  @Nested
+  class ConstructorGetterTests{
 
-    //Act
-    appleStock.addNewSalesPrice(price2);
-    appleStock.addNewSalesPrice(price3);
-    appleStock.addNewSalesPrice(price4);
+    @Test
+    void testGetSymbol() {
+      //Arrange and Act
+      String symbol = appleStock.getSymbol();
+      //Assert
+      assertEquals("AAPL", symbol);
+    }
+    @Test
+    void testGetCompany() {
+      //Arrange and Act
+      String company = appleStock.getCompany();
+      //Assert
+      assertEquals("Apple Inc.", appleStock.getCompany());
+    }
+    @Test
+    void testGetSalesPriceWithReturnsInitialPrice() {
+      //Arrange and Act
+      BigDecimal salesPrice = appleStock.getSalesPrice();
+      //Assert
+      assertEquals(initialPrice, salesPrice);
+    }
 
-    //Assert
-    assertEquals(price4, appleStock.getSalesPrice());
-    assertEquals(4, appleStock.getPriceHistory().size());
   }
-  //Price History tests
-  @Test
-  void testGetPriceHistoryInitialSize() {
-    //Arrange
-    var history = appleStock.getPriceHistory();
-    //Assert
-    assertEquals(1, history.size());
-    assertEquals(initialPrice, history.get(0));
-  }
-  @Test
-  void testGetPriceHistoryAfterAddingPrice() {
-    //Arrange
-    BigDecimal price2 = new BigDecimal("160.50");
-    BigDecimal price3 = new BigDecimal("164.50");
-    appleStock.addNewSalesPrice(price2);
-    appleStock.addNewSalesPrice(price3);
-    //Act
-    var history = appleStock.getPriceHistory();
-    //Assert
-    assertEquals(3, history.size());
-    assertEquals(initialPrice, history.get(0));
-    assertEquals(price2, history.get(1));
-    assertEquals(price3, history.get(2));
-  }
-  @Test
-  void testGetPriceHistoryReturnsNewPrice() {
-    //Arrange and Act
-    var history1 = appleStock.getPriceHistory();
-    var history2 = appleStock.getPriceHistory();
-    //Assert
-    assertNotSame(history1, history2);
-  }
-  @Test
-  void testWithEqualSameSymbol() {
-    //Arrange and Act
-    Stock stock1= new Stock("AAPL", "Apple Inc.", new BigDecimal("150.50"));
-    Stock stock2 = new Stock("AAPL", "Apple Inc.", new BigDecimal("155.50"));
-    //Assert
-    assertEquals(stock1, stock2);
-  }
-  @Test
-  void testEqualsWithDifferentSymbol() {
-    //Arrange and Act
-    Stock stock1= new Stock("AAPL", "Apple Inc.", new BigDecimal("150.50"));
-    Stock stock2= new Stock("GOOGL", "Alphabet Inc..", new BigDecimal("2800.00"));
-    assertNotEquals(stock1, stock2);
-  }
+  @Nested
+  class AddTests{
+    @Test
+    void testAddNewSalesPriceWithValidPrice() {
+      //Arrange
+      BigDecimal newPrice = new BigDecimal("160.50");
+      //Act
+      appleStock.addNewSalesPrice(newPrice);
+      //Assert
+      assertEquals(newPrice, appleStock.getSalesPrice());
 
+
+    }
+
+    @Test
+    void testaddNewSalesPriceWithZeroPrice() {
+      //Arrange and Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> appleStock.addNewSalesPrice(BigDecimal.ZERO));
+      //Assert
+      assertTrue(exception.getMessage().contains("Sales"));
+    }
+    @Test
+    void testaddNewSalesPriceWithNegativePrice() {
+      //Arrange and Act
+      IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+          () -> appleStock.addNewSalesPrice(new BigDecimal("-150.50")));
+
+      //Assert
+      assertTrue(exception.getMessage().contains("Sales"));
+
+    }
+    @Test
+    void testAddMultiplePrices() {
+      BigDecimal price2 = new BigDecimal("158.50");
+      BigDecimal price3 = new BigDecimal("160.50");
+      BigDecimal price4 = new BigDecimal("164.50");
+
+      //Act
+      appleStock.addNewSalesPrice(price2);
+      appleStock.addNewSalesPrice(price3);
+      appleStock.addNewSalesPrice(price4);
+
+      //Assert
+      assertEquals(price4, appleStock.getSalesPrice());
+      assertEquals(4, appleStock.getPriceHistory().size());
+    }
+  }
+  @Nested
+  class GetPriceHistoryTests{
+    //Price History tests
+    @Test
+    void testGetPriceHistoryInitialSize() {
+      //Arrange
+      var history = appleStock.getPriceHistory();
+      //Assert
+      assertEquals(1, history.size());
+      assertEquals(initialPrice, history.get(0));
+    }
+    @Test
+    void testGetPriceHistoryAfterAddingPrice() {
+      //Arrange
+      BigDecimal price2 = new BigDecimal("160.50");
+      BigDecimal price3 = new BigDecimal("164.50");
+      appleStock.addNewSalesPrice(price2);
+      appleStock.addNewSalesPrice(price3);
+      //Act
+      var history = appleStock.getPriceHistory();
+      //Assert
+      assertEquals(3, history.size());
+      assertEquals(initialPrice, history.get(0));
+      assertEquals(price2, history.get(1));
+      assertEquals(price3, history.get(2));
+    }
+
+    @Test
+    void testGetPriceHistoryReturnsNewPrice() {
+      //Arrange and Act
+      var history1 = appleStock.getPriceHistory();
+      var history2 = appleStock.getPriceHistory();
+      //Assert
+      assertNotSame(history1, history2);
+    }
+    @Test
+    void testWithEqualSameSymbol() {
+      //Arrange and Act
+      Stock stock1= new Stock("AAPL", "Apple Inc.", new BigDecimal("150.50"));
+      Stock stock2 = new Stock("AAPL", "Apple Inc.", new BigDecimal("155.50"));
+      //Assert
+      assertEquals(stock1, stock2);
+    }
+    @Test
+    void testEqualsWithDifferentSymbol() {
+      //Arrange and Act
+      Stock stock1= new Stock("AAPL", "Apple Inc.", new BigDecimal("150.50"));
+      Stock stock2= new Stock("GOOGL", "Alphabet Inc..", new BigDecimal("2800.00"));
+      assertNotEquals(stock1, stock2);
+    }
+
+  }
 
 }
 
