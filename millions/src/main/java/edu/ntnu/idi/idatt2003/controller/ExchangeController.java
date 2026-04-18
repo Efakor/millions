c1d66303a4522f8b6f5a2ffc58f16032b76c21ba
Vanessa Efakor Akosua Amedi <veamedi@stud.ntnu.no>
2026-04-18 19:52:58 +0200

    [FIX] Add getExchange() getter in ExchangeController

--- millions/src/main/java/edu/ntnu/idi/idatt2003/controller/ExchangeController.java
+++ millions/src/main/java/edu/ntnu/idi/idatt2003/controller/ExchangeController.java
@@ -1,24 +1,27 @@
 package edu.ntnu.idi.idatt2003.controller;
 
-
 import edu.ntnu.idi.idatt2003.model.Exchange;
 import edu.ntnu.idi.idatt2003.model.Share;
 import edu.ntnu.idi.idatt2003.model.Stock;
-
-import java.util.List;
 import java.io.File;
 import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * Controller for Exchange operations.
  * Handles buying, selling, and market operations.
  */
-
 public class ExchangeController {
   private final Exchange exchange;
+
   public ExchangeController(Exchange exchange) {
     this.exchange = exchange;
-
   }
+
+  public Exchange getExchange() {
+    return exchange;
+  }
+
   public void buy(String symbol, BigDecimal qty) {
     throw new UnsupportedOperationException("Not supported yet.");
 
